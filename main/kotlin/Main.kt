0f8b5d4f62df8a009f81ff40e725e5202f71a85e
Denis Manikhin <dmanikhine@gmail.com>
2021-10-25 09:28:23 +0200

    Add class RockPaperScissors and  enum class GameItem

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -5,3 +5,24 @@ fun main(args: Array<String>) {
     println("Let's have a fun!!!")
     println()
 }
+
+public class RockPaperScissors {
+
+    enum class GameItem(val enumString: String) {
+        ROCK("rock"),
+        PAPER("paper"),
+        SCISSORS("scissors");
+
+        companion object {
+            init {
+                ROCK.listOfVictims.add(SCISSORS)
+                PAPER.listOfVictims.add(ROCK)
+                SCISSORS.listOfVictims.add(PAPER)
+            }
+        }
+        private val listOfVictims: MutableList<GameItem> = ArrayList()
+        fun isVictim(otherChoice: GameItem): Boolean {
+            return listOfVictims.contains(otherChoice)
+        }
+    }
+}
