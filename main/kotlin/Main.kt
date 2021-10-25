4b11b53896c0c327e56be13442d5a0df57b4cf9f
Denis Manikhin <dmanikhine@gmail.com>
2021-10-25 09:33:59 +0200

    Add fun printItems()

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -4,9 +4,21 @@ fun main(args: Array<String>) {
     println("We are going to play: Rock, Paper, Scissors!")
     println("Let's have a fun!!!")
     println()
+    val rockPaperScissors = RockPaperScissors()
+    rockPaperScissors.printItems()
 }
 
 public class RockPaperScissors {
+    
+    fun printItems() {
+        val stringBuilder = StringBuilder()
+        stringBuilder.append("Please input your choice of one of the following:")
+        for (choice in GameItem.values()) {
+            stringBuilder.append(" ")
+            stringBuilder.append(choice.enumString)
+        }
+        println(stringBuilder.toString())
+    }
 
     enum class GameItem(val enumString: String) {
         ROCK("rock"),
