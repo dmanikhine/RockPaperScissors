9842c2c2f378382da20e110e7ba299365a9c93e0
Denis Manikhin <dmanikhine@gmail.com>
2021-10-25 09:40:26 +0200

    Add fun getUserChoice

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -1,15 +1,33 @@
 // I'm gonna go build my own theme park with blackjack and ...... in
 
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.util.*
+
+
 fun main(args: Array<String>) {
     println("We are going to play: Rock, Paper, Scissors!")
     println("Let's have a fun!!!")
     println()
     val rockPaperScissors = RockPaperScissors()
-    rockPaperScissors.printItems()
+    rockPaperScissors.execute();
 }
 
 public class RockPaperScissors {
-    
+    fun execute() {
+        playGame(InputStreamReader(System.`in`))
+    }
+
+    fun playGame(inputStreamReader: InputStreamReader) {
+        BufferedReader(inputStreamReader).use { br ->
+            printItems()
+            val userChoice: GameItem = getUserChoice(br)
+            println("")
+            println("You picked:  $userChoice")
+        }
+    }
+
     fun printItems() {
         val stringBuilder = StringBuilder()
         stringBuilder.append("Please input your choice of one of the following:")
@@ -18,6 +36,22 @@ public class RockPaperScissors {
             stringBuilder.append(choice.enumString)
         }
         println(stringBuilder.toString())
+    }
+
+    fun getUserChoice(br:BufferedReader): GameItem {
+        try {
+            while(true) {
+                val userChoiceString = br.readLine()
+                if (!GameItem.values().any { i -> i.enumString == userChoiceString }) {
+                    println("Please enter one of the valid options.")
+                } else {
+                    return GameItem.valueOf(userChoiceString.uppercase(Locale.getDefault()))
+                }
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+            throw RuntimeException("There was an error while reading from input.", e)
+        }
     }
 
     enum class GameItem(val enumString: String) {
